06f97fb623e21bf10bffc37ae918dd3e7c1d3a43
root <root@localhost.localdomain>
2021-02-01 23:28:23 +0900

    Add #2

--- src/App.js
+++ src/App.js
@@ -5,21 +5,54 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Subject></Subject>
+                <Subject title="WEB" subTitle="World Wide Web!"></Subject>
+                <TOC></TOC>
+                <MainText title="HTML" text="HTML is HyperText Markup Language."></MainText>
             </div>
         );
     }
 }
 
 class Subject extends Component {
+    __construct() {
+        console.log(this);
+    }
+
     render() {
         return (
             <header>
-                <h1>WEB</h1>
-                World Wide Web!
+                <h1>{this.props.title}</h1>
+                {this.props.subTitle}
             </header>
         );
     }
 }
+
+class TOC extends Component {
+    render() {
+        return (
+            <nav>
+                <ui>
+                    <li><a href="1.html">HTML</a></li>
+                    <li><a href="2.html">CSS</a></li>
+                    <li><a href="3.html">JavaScript</a></li>
+                </ui>
+            </nav>
+        );
+    }
+}
+
+class MainText extends Component {
+    render() {
+        return (
+            <article>
+                <h2>{this.props.title}</h2>
+                {this.props.text}
+            </article>
+        );
+    }
+}
+
+
 
 export default App;
