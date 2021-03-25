5832673774d1693857d53fc678db20f40486fcf6
PetERPAN17.otsdPJT <81074428+PetERPAN17-outside-PJT@users.noreply.github.com>
2021-03-25 19:35:18 +0900

    Update #6 (#7)
    Co-authored-by: root <root@localhost.localdomain>

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    constructor(props) {
-        super(props);
-
-        this.lists = [];
-
-        let setData = (element) => (
-            this.lists.push(<li key={element.id}><a href={element.id + '.html'}>{element.title}</a></li>)
-        )
-
-        props.data.forEach(
-            (element) => setData(element)
-        );
-    }
-
     render() {
+
+        const data = this.props.data;
+
         return (
             <nav>
-                <ul>
-                    {this.lists}
-                </ul>
+                {data.map((value, index) => {
+                    return <li key={'TOC-li-' + index}><a href={value.id + '.html'}>{value.title}</a></li>
+                })}
             </nav>
         );
     }
