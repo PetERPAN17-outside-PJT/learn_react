0f7d9c7b131a67c3d15f645d7914bbdb321d4a93
root <root@localhost.localdomain>
2021-03-30 18:08:04 +0900

    Update #8

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class Subject extends Component {
+    constructor(props) {
+        super(props);
+        this.aClick = this.aClick.bind(this);
+    }
+
+    aClick(e) {
+        e.preventDefault();
+        this.props.onChangePage(e);
+    }
+
     render() {
         return (
             <header>
-                <h1><a href="/">{this.props.title}</a></h1>
+                <h1><a href="/"
+                    onClick={this.aClick}
+                >{this.props.title} </a></h1>
                 {this.props.sub}
             </header>
         );
